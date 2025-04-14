a564a0c97c25e368ee841876da6c8b9e794c97d2
Francisco José Morcillo Suárez <francisco.morcillo89@gmail.com>
2025-04-14 23:36:43 +0200

    Añadidos los ejercicios del día 3 de la semana 1

---
+++ Curso1/Ejercicios/Semana1/dia3/ejercicio3/app/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        concatenarCadenas("HOLA","mundo");
+    }
+
+    public static void concatenarCadenas(String cadenaMayusculas, String cadenaMinusculas){
+
+        System.out.println(cadenaMinusculas + " " + cadenaMayusculas);
+
+    }
+}
