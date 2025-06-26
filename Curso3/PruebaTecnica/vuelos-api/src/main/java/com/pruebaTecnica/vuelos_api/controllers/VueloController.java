7626106669d819813ff5aa4f044093c0adeb93fb
Francisco José Morcillo Suárez <francisco.morcillo89@gmail.com>
2025-06-26 21:53:16 +0200

    añadidos GET (listado y por id) en VueloController

--- Curso3/PruebaTecnica/vuelos-api/src/main/java/com/pruebaTecnica/vuelos_api/controllers/VueloController.java
+++ Curso3/PruebaTecnica/vuelos-api/src/main/java/com/pruebaTecnica/vuelos_api/controllers/VueloController.java
@@ -1,7 +1,11 @@
 package com.pruebaTecnica.vuelos_api.controllers;
 
+import com.pruebaTecnica.vuelos_api.dtos.VueloDTO;
 import com.pruebaTecnica.vuelos_api.services.VueloServiceInterface;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/vuelos")
@@ -13,5 +17,16 @@ public class VueloController {
         this.serviceInterface = serviceInterface;
     }
 
+    // http:localhost:8080/vuelos
+    @GetMapping()
+    public ResponseEntity<List<VueloDTO>> obtenerTodosLosVuelos(){
+        return serviceInterface.obtenerTodosLosVuelos();
+    }
+
+    // http:localhost:8080/vuelos/id    en id poner el id que quieres buscar
+    @GetMapping("{id}")
+    public ResponseEntity<VueloDTO> buscarVuelo(@PathVariable Integer id){
+        return serviceInterface.buscarVuelo(id);
+    }
 
 }
